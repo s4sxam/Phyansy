c28480978691faa68ab99ddabca5cbfa0e653b42
Tanay ⌬ <tanayguha07@gmail.com>
2026-06-04 12:42:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@
 import { initPageController }    from './js/modules/pageController.js';
 import { initSettingsController } from './js/modules/settingsController.js';
 import { initLangController }    from './js/modules/langController.js';
-import { initLangPicker }        from './js/modules/langPickerController.js';
 import { initThemeController, toggleTheme } from './js/modules/themeController.js';
 import { initBgRenderer }        from './js/modules/bgRenderer.js';
 import { initFavicon }           from './js/modules/faviconController.js';
@@ -92,7 +91,7 @@ function injectSectionIcons() {
     initBgRenderer();
     wireThemeToggle();
     injectSectionIcons();
-    initLangPicker();     // ← Language globe button + picker panel
+
     initPageController();
     initSearch();   // ← Advanced search engine (global + fuzzy)
   };
